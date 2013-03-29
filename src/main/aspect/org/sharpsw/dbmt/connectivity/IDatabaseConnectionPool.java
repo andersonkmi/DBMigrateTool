1bf16679dbc785b307ad8575d549b1501938b015
Anderson Ito <andersonkmi@acm.org>
2013-03-29 20:16:14 -0300

    Removed connection pool aspects. Not needed.

--- src/main/aspect/org/sharpsw/dbmt/connectivity/IDatabaseConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package org.sharpsw.dbmt.connectivity;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDatabaseConnectionPool {
-	public Connection getConnection(String url) throws SQLException;	
-	public void surrenderConnection(Connection connection);
-}
