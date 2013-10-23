68b6bffa4d663259e549f6e6b645c3b86250474e
andersonkmi <andersonkmi@acm.org>
2013-10-22 23:40:45 -0200

    Bunch of modifications

--- src/main/java/org/sharpsw/dbmt/connectivity/DBConnectionCreator.java
+++ src/main/java/org/sharpsw/dbmt/connectivity/DBConnectionCreator.java
@@ -1,48 +1,45 @@
 package org.sharpsw.dbmt.connectivity;
 
+import org.sharpsw.dbmt.config.IDatabaseConfiguration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.sharpsw.dbmt.config.IDatabaseConfiguration;
-
 public class DBConnectionCreator {
-	private IDatabaseConfiguration configuration;
-	
-	@SuppressWarnings("unused")
-	private DBConnectionCreator() {}
-	
-	public DBConnectionCreator(IDatabaseConfiguration configuration) throws DBConnectionDriverLoadException {
-		this.configuration = configuration;
-		String driverClass = this.configuration.getDriverClassName();
-		
-		try {
-			Class.forName(driverClass);
-		} catch (ClassNotFoundException classNotFoundExc) {
-			StringBuffer message = new StringBuffer();
-			message.append("Error when loading the driver: '").append(driverClass).append("'");
-			throw new DBConnectionDriverLoadException(message.toString(), classNotFoundExc);
-		} catch (ExceptionInInitializerError error) {
-			StringBuffer message = new StringBuffer();
-			message.append("Error when loading the driver: '").append(driverClass).append("'");
-	
-			throw new DBConnectionDriverLoadException(message.toString(), error);
-		} catch (LinkageError linkageErr) {
-			StringBuffer message = new StringBuffer();
-			message.append("Error when loading the driver: '").append(driverClass).append("'");
-			throw new DBConnectionDriverLoadException(message.toString(), linkageErr);
-		} 
-	}
-	
-	public Connection getConnection() throws DBConnectionCreateException {
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(this.configuration.getConnectionString());
-			return connection;
-		} catch (SQLException exception) {
-			StringBuffer message = new StringBuffer();
-			message.append("Error when creating a new connection to the database.");
-			throw new DBConnectionCreateException(message.toString(), exception);
-		}
-	}
+    private IDatabaseConfiguration configuration;
+
+    public DBConnectionCreator(IDatabaseConfiguration configuration) throws DBConnectionDriverLoadException {
+        this.configuration = configuration;
+        String driverClass = this.configuration.getDriverClassName();
+
+        try {
+            Class.forName(driverClass);
+        } catch (ClassNotFoundException classNotFoundExc) {
+            StringBuilder message = new StringBuilder();
+            message.append("Error when loading the driver: '").append(driverClass).append("'");
+            throw new DBConnectionDriverLoadException(message.toString(), classNotFoundExc);
+        } catch (ExceptionInInitializerError error) {
+            StringBuilder message = new StringBuilder();
+            message.append("Error when loading the driver: '").append(driverClass).append("'");
+
+            throw new DBConnectionDriverLoadException(message.toString(), error);
+        } catch (LinkageError linkageErr) {
+            StringBuilder message = new StringBuilder();
+            message.append("Error when loading the driver: '").append(driverClass).append("'");
+            throw new DBConnectionDriverLoadException(message.toString(), linkageErr);
+        }
+    }
+
+    public Connection getConnection() throws DBConnectionCreateException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(this.configuration.getConnectionString());
+            return connection;
+        } catch (SQLException exception) {
+            StringBuilder message = new StringBuilder();
+            message.append("Error when creating a new connection to the database.");
+            throw new DBConnectionCreateException(message.toString(), exception);
+        }
+    }
 }
