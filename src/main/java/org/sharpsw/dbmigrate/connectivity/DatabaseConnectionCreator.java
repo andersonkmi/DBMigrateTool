9903d68449a0b2f5b74ed6dc36c8b90609014de6
andersonkmi <andersonkmi@acm.org>
2014-04-20 21:16:53 -0300

    Refactorings

--- src/main/java/org/sharpsw/dbmigrate/connectivity/DatabaseConnectionCreator.java
+++ src/main/java/org/sharpsw/dbmigrate/connectivity/DatabaseConnectionCreator.java
@@ -1,5 +1,6 @@
 package org.sharpsw.dbmigrate.connectivity;
 
+import org.apache.log4j.Logger;
 import org.sharpsw.dbmigrate.config.DatabaseConfig;
 
 import java.sql.Connection;
@@ -7,21 +8,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionCreator {
+    private static final Logger logger = Logger.getLogger(DatabaseConnectionCreator.class);
 
     public Connection getConnection(final DatabaseConfig configuration) throws DatabaseConnectionCreateException, DatabaseConnectionDriverLoadException {
-        Connection connection = null;
+        if(logger.isDebugEnabled()) {
+            logger.debug(String.format("Creating a new database connection for configuration: '%s'", configuration.getConnectionString()));
+        }
+
         try {
             Class.forName(configuration.getDriverClassName());
-            connection = DriverManager.getConnection(configuration.getConnectionString());
+            Connection connection = DriverManager.getConnection(configuration.getConnectionString());
+
+            logger.info("Database connection created successfully");
             return connection;
         } catch (SQLException exception) {
-            StringBuilder message = new StringBuilder();
-            message.append("Error when creating a new connection to the database.");
-            throw new DatabaseConnectionCreateException(message.toString(), exception);
+            logger.error(String.format("Error when creating a new database connection: '%s", exception.getMessage()), exception);
+            throw new DatabaseConnectionCreateException(String.format("Error when creating a new connection to the database: %s", exception.getMessage()), exception);
         } catch (ClassNotFoundException classNotFoundExc) {
-            StringBuilder message = new StringBuilder();
-            message.append("Error when loading the driver: '").append(configuration.getDriverClassName()).append("'");
-            throw new DatabaseConnectionDriverLoadException(message.toString(), classNotFoundExc);
+            logger.error(String.format("Error when loading the database driver '%s", configuration.getDriverClassName()), classNotFoundExc);
+            throw new DatabaseConnectionDriverLoadException(String.format("Error when loading driver '%s'", configuration.getDriverClassName()), classNotFoundExc);
         }
     }
 }
