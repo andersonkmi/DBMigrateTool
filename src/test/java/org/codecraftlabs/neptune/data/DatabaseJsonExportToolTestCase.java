b56c67a78a7dd5aa740e895476068b96af7be56b
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-07-26 13:18:23 +0200

    Modifications and docker support for integration tests

---
+++ src/test/java/org/codecraftlabs/neptune/data/DatabaseJsonExportToolTestCase.java
@@ -0,0 +1,35 @@
+package org.codecraftlabs.neptune.data;
+
+import org.codecraftlabs.neptune.config.DatabaseConfig;
+import org.codecraftlabs.neptune.config.PostgreSQLConfiguration;
+import org.codecraftlabs.neptune.utility.DatabaseJsonExportTool;
+import org.codecraftlabs.neptune.utility.DatabaseSchemaExportException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public class DatabaseJsonExportToolTestCase {
+	private DatabaseConfig config;
+
+	@Container
+	public GenericContainer<?> postgres = new GenericContainer<>(new DockerImageName("maui/mauidb:latest").toString()).withExposedPorts(5432).waitingFor(Wait.forListeningPort());
+
+	@BeforeEach
+	public void setUp() {
+		String address = postgres.getHost();
+		Integer port = postgres.getFirstMappedPort();
+		config = new PostgreSQLConfiguration(address, port, "postgres", "postgres", "maui");
+	}
+
+	@Test
+	public void testCase001() throws DatabaseSchemaExportException {
+		DatabaseJsonExportTool tool = new DatabaseJsonExportTool();
+		String json = tool.export(config);
+		System.out.println(json);
+	}
+}
